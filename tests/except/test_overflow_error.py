17b4b055f4ecb9b5a40ab7af287ec4245714df14
Julien Palard <julien@palard.fr>
2020-06-25 22:53:11 +0200

    Getting 'Numerical result out of range' instead of 'Result too large'.

--- tests/except/test_overflow_error.py
+++ tests/except/test_overflow_error.py
@@ -7,7 +7,10 @@ def test_overflow_error():
     except Exception:
         friendly_traceback.explain(redirect="capture")
     result = friendly_traceback.get_output()
-    assert "OverflowError: (34, 'Result too large')" in result
+    assert (
+        "OverflowError: (34, 'Result too large')" in result
+        or "OverflowError: (34, 'Numerical result out of range')" in result
+    )
     if friendly_traceback.get_lang() == "en":
         assert "OverflowError is raised when the result" in result
     return result
