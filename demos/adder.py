19e23c9181b0ee65d8cf9b1e8717ce7952117688
André Roberge <andre.roberge@gmail.com>
2020-07-26 09:59:31 -0300

    Adding command line argument to demo to illustrate usage (tied to documentation).

--- demos/adder.py
+++ demos/adder.py
@@ -1,12 +1,18 @@
 # Demonstration of a program that uses command line arguments
-import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("numbers", nargs="*", help="List of numbers to add.")
+parser.add_argument("--to_int", help="Converts the sum to integer", action="store_true")
+
 
 total = 0
+args = parser.parse_args()
 
-for arg in sys.argv[1:]:
-    total += float(arg)
+for number in args.numbers:
+    total += float(number)
 
-if int(total) == total:
+if int(total) == total and args.to_int:
     total = int(total)
 
 print("The sum is", total)
