e47093140a5fff44d2665cac5ac72bd4109080ae
André Roberge <andre.roberge@gmail.com>
2020-11-14 07:16:17 -0400

    Adding test for running source with args.

---
+++ tests/adder.py
@@ -0,0 +1,18 @@
+# Demonstration of a program that uses command line arguments
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("numbers", nargs="*", help="List of numbers to add.")
+parser.add_argument("--to_int", help="Converts the sum to integer", action="store_true")
+
+
+total = 0
+args = parser.parse_args()
+
+for number in args.numbers:
+    total += float(number)
+
+if int(total) == total and args.to_int:
+    total = int(total)
+
+print(f"The sum is {total}.")
---
+++ tests/unit/test_source_with_args.py
@@ -0,0 +1,30 @@
+"""Tests of running a program that uses command line arguments.
+"""
+
+import subprocess
+
+def run(*args):
+    proc = subprocess.run(
+        [
+            "python",
+            "-m",
+            "friendly_traceback",
+            "tests/adder.py",
+            "--",
+            *args,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    return proc.stdout
+
+
+def test_args_float():
+    result = run("1", "2.5", "3")
+    assert "The sum is 6.5." in result
+
+
+def test_args_to_int():
+    result = run("1", "2.5", "3", "--to_int")
+    assert "The sum is 6." in result
