2af98412bd82f3d2228827e618d69a2d2ebc7a8c
André Roberge <andre.roberge@gmail.com>
2020-12-29 08:15:47 -0400

    Improving the organization of SyntaxError cases.

--- tests/syntax/catch_syntax_error.py
+++ tests/syntax/catch_syntax_error.py
@@ -3,132 +3,27 @@ import sys
 import pytest
 
 import friendly_traceback
+from syntax_errors_descriptions import descriptions
+
+
+if sys.version_info < (3, 8):
+    descriptions["raise_syntax_error_walrus"]["cause"] = "walrus operator"
+    descriptions["raise_syntax_error55"]["cause"] = "walrus operator"
+if sys.version_info >= (3, 9):
+    descriptions["raise_syntax_error66"]["cause"] = "expected an indented block"
 
 friendly_traceback.set_lang("en")
 
-# Format of the dict below
-# filename : "partial content of the Friendly traceback information"
 
-causes = {
-    "raise_indentation_error1": "expected to begin a new indented block",
-    "raise_indentation_error2": "does not match the indentation of the previous line",
-    "raise_indentation_error3": "not aligned vertically with another block of code",
-    "raise_tab_error": "A `TabError` indicates that you have used",
-    "raise_syntax_error1": "assign a value to the Python keyword `def`",
-    "raise_syntax_error2": "`if` but forgot to add a colon `:`",
-    "raise_syntax_error3": "wrote a `while` loop but",
-    "raise_syntax_error4": "wrote `else if` instead",
-    "raise_syntax_error5": "wrote `elseif` instead",
-    "raise_syntax_error6": "tried to define a function or method",
-    "raise_syntax_error7": "tried to define a function or method",
-    "raise_syntax_error8": "tried to define a function or method",
-    "raise_syntax_error9": "is or includes an actual object of type `int`",
-    "raise_syntax_error10": "is or includes an actual object of type `int`",
-    "raise_syntax_error11": "from turtle import pen",
-    "raise_syntax_error12": "EOL while scanning string literal",
-    "raise_syntax_error13": "`None` is a constant in Python",
-    "raise_syntax_error14": "`__debug__` is a constant in Python",
-    "raise_syntax_error15": "The closing parenthesis `)` on",
-    "raise_syntax_error16": "The opening parenthesis `(` on line",
-    "raise_syntax_error17": "The opening parenthesis `(` on line",
-    "raise_syntax_error18": "The closing square bracket `]` on line",
-    "raise_syntax_error19": "The closing square bracket `]` on line",
-    "raise_syntax_error20": "In older version of Python, `print` was a keyword",
-    "raise_syntax_error21": "You tried to use the Python keyword",
-    "raise_syntax_error22": "The Python keyword `break` can only be used",
-    "raise_syntax_error23": "The Python keyword `continue` can only be used",
-    "raise_syntax_error24": "quote inside a string",
-    "raise_syntax_error25": "forgot a comma",
-    "raise_syntax_error26": "forgot a comma",
-    "raise_syntax_error27": "forgot a comma",
-    "raise_syntax_error28": "forgot a comma",
-    "raise_syntax_error29": "forgot a comma",
-    "raise_syntax_error30": "function call and is not simply the name of a variable",
-    "raise_syntax_error31": "function call and not the name of a variable",
-    "raise_syntax_error32": "used an equal sign `=` instead of a colon `:`",
-    "raise_syntax_error33": "define functions with only positional arguments",
-    "raise_syntax_error34": "call functions with only positional arguments",
-    "raise_syntax_error35": "a string prefixed by the letter f,",
-    "raise_syntax_error36": "The opening square bracket `[`",
-    "raise_syntax_error37": "The opening square bracket `[`",
-    "raise_syntax_error38": "is a variable defined outside a function.",
-    "raise_syntax_error39": "keyword `pass` as an attribute",
-    "raise_syntax_error40": "using the continuation character",
-    "raise_syntax_error41": "where `invalid` is not a valid variable name in Python",
-    "raise_syntax_error42": "unicode character",
-    "raise_syntax_error43": "as an argument in the definition of a function",
-    "raise_syntax_error44": "as an argument in the definition of a function",
-    "raise_syntax_error45": "as an argument in the definition of a function",
-    "raise_syntax_error46": "as an argument in the definition of a function",
-    "raise_syntax_error47": "instead of deleting the function's name",
-    "raise_syntax_error48": "before declaring it as a global variable.",
-    "raise_syntax_error49": "You used the variable `r`",
-    "raise_syntax_error50": "before declaring it as a nonlocal variable.",
-    "raise_syntax_error51": "You assigned a value to the variable `s`",
-    "raise_syntax_error52": "is or includes an actual object of type `set`",
-    "raise_syntax_error53": "is or includes an actual object of type `dict`",
-    "raise_syntax_error54": "is or includes an actual object",
-    "raise_syntax_error55": "`True` is a constant in Python",  # see below
-    "raise_syntax_error56": "only used to assign a value to a variable",
-    "raise_syntax_error57": "use the function `repr(x)`.",
-    "raise_syntax_error58": "generator expression instead of the name of a variable",
-    "raise_syntax_error59": "variable = object if condition else other_object",
-    "raise_syntax_error60": "You used `x` as a parameter for a function",
-    "raise_syntax_error61": "You declared `xy` as being both a global and nonlocal",
-    "raise_syntax_error62": "nonlocal variable but it cannot be found.",
-    "raise_syntax_error63": "You used the nonlocal keyword at a module level",
-    "raise_syntax_error64": "keyword argument should appear only once in a function definition",
-    "raise_syntax_error65": "keyword argument should appear only once in a function call",
-    "raise_syntax_error66": "it reached the end of the file and expected more content.",  # see below
-    "raise_syntax_error67": "In older version of Python, `print` was a keyword",
-    "raise_syntax_error68": "copy-pasted code from an interactive interpreter",
-    "raise_syntax_error69": "`pip` is a command that needs to run in a terminal",
-    "raise_syntax_error70": "`pip` is a command that needs to run in a terminal",
-    "raise_syntax_error71": "dot `.` followed by `(`",
-    "raise_syntax_error72": "expression that has an f-string",
-    "raise_syntax_error73": "trying to raise an exception using Python 2 syntax.",
-    "raise_syntax_error74": "must add parentheses enclosing",
-    "raise_syntax_error75": "fancy unicode quotation mark",
-    "raise_syntax_error76": "`=` instead of an equality operator `==` with an `if` statement",
-    "raise_syntax_error77": "`=` instead of an equality operator `==` with an `elif` statement",
-    "raise_syntax_error78": "You used an assignment operator `=`",
-    "raise_syntax_error79": "forgot a comma",
-    "raise_syntax_error80": "Valid names cannot begin with a number",
-    "raise_syntax_error81": "The opening parenthesis `(` on line 5 is not closed",
-    "raise_syntax_error82": "Perhaps you forgot a multiplication operator",
-    "raise_syntax_error83": "Did you mean `a_b`",
-    "raise_syntax_error84": "The star operator",
-    "raise_syntax_error85": "The double star operator",
-    "raise_syntax_error86": "You can only use a `return`",
-    "raise_syntax_error87": "Seriously",
-    "raise_syntax_error88": "replace `*` by either `*arguments` or ",
-    "raise_syntax_error89": "Did you mean `3.0 j`",
-    "raise_syntax_error90": "you must import specific named features",
-    "raise_syntax_error91": "Did you mean `division`",
-    "raise_syntax_error92": "print_function",
-    "raise_syntax_error93": "from __future__ import braces",
-    "raise_syntax_error94": "It must appear at the beginning of the file.",
-}
-
-if sys.version_info < (3, 8):
-    causes["raise_syntax_error_walrus"] = "walrus operator"
-    causes["raise_syntax_error55"] = "walrus operator"
-if sys.version_info >= (3, 9):
-    causes["raise_syntax_error66"] = "expected an indented block"
-
-
-@pytest.mark.parametrize("filename", causes.keys())
+@pytest.mark.parametrize("filename", descriptions.keys())
 def test_syntax_errors(filename):
-    cause = causes[filename]
+    cause = descriptions[filename]["cause"]
+
     try:
-        try:
-            exec("from . import %s" % filename)  # for pytest
-        except ImportError:
-            exec("import %s" % filename)
+        exec("from . import %s" % filename)
     except Exception:
         friendly_traceback.explain_traceback(redirect="capture")
     result = friendly_traceback.get_output()
-    
 
     if "syntax" in filename and not "indented" in cause:
         assert "SyntaxError" in result, (
@@ -140,5 +35,6 @@ def test_syntax_errors(filename):
         )
     else:
         assert "TabError" in result, "TabError identified incorrectly; %s" % filename
+
     unwrapped_result = " ".join(result.split())
     assert cause in unwrapped_result, "\nExpected to see: %s\nIn: %s" % (cause, result)
--- tests/trb_syntax_common.py
+++ tests/trb_syntax_common.py
@@ -5,8 +5,13 @@
 import os
 import sys
 from contextlib import redirect_stderr
-
 import friendly_traceback
+from syntax_errors_descriptions import descriptions
+
+if sys.version_info < (3, 8):
+    descriptions["raise_syntax_error_walrus"][
+        "cause"
+    ] = "Walrus operator does not exist - yet"
 
 
 def write(text):
@@ -26,103 +31,6 @@ def make_title(text, format="pre"):
         sys.exit()
 
 
-all_imports = {
-    "IndentationError - 1: expected an indented block": "raise_indentation_error1",
-    "IndentationError - 2: unexpected indent": "raise_indentation_error2",
-    "IndentationError - 3: unindent does not match ...": "raise_indentation_error3",
-    "TabError": "raise_tab_error",
-    "SyntaxError - Assign to keyword": "raise_syntax_error1",
-    "SyntaxError - Missing colon - 1": "raise_syntax_error2",
-    "SyntaxError - Missing colon - 2": "raise_syntax_error3",
-    "SyntaxError - elif, not else if": "raise_syntax_error4",
-    "SyntaxError - elif, not elseif": "raise_syntax_error5",
-    "SyntaxError - malformed def statment - 1": "raise_syntax_error6",
-    "SyntaxError - malformed def statment - 2": "raise_syntax_error7",
-    "SyntaxError - malformed def statment - 3": "raise_syntax_error8",
-    "SyntaxError - can't assign to literal - 1": "raise_syntax_error9",
-    "SyntaxError - can't assign to literal - 2": "raise_syntax_error10",
-    "SyntaxError - can't assign to literal - 3": "raise_syntax_error52",
-    "SyntaxError - can't assign to literal - 4": "raise_syntax_error53",
-    "SyntaxError - can't assign to literal - 5": "raise_syntax_error54",
-    "SyntaxError - import X from Y": "raise_syntax_error11",
-    "SyntaxError - EOL while scanning string literal": "raise_syntax_error12",
-    "SyntaxError - assignment to keyword (None)": "raise_syntax_error13",
-    "SyntaxError - assignment to keyword (__debug__)": "raise_syntax_error14",
-    "SyntaxError - unmatched closing parenthesis": "raise_syntax_error15",
-    "SyntaxError - unclosed parenthesis- 1": "raise_syntax_error16",
-    "SyntaxError - unclosed parenthesis - 2": "raise_syntax_error17",
-    "SyntaxError - mismatched brackets": "raise_syntax_error18",
-    "SyntaxError - mismatched brackets - 2": "raise_syntax_error19",
-    "SyntaxError - print is a function": "raise_syntax_error20",
-    "SyntaxError - Python keyword as function name": "raise_syntax_error21",
-    "SyntaxError - break outside loop": "raise_syntax_error22",
-    "SyntaxError - continue outside loop": "raise_syntax_error23",
-    "SyntaxError - quote inside a string": "raise_syntax_error24",
-    "SyntaxError - missing comma in a dict": "raise_syntax_error25",
-    "SyntaxError - missing comma in a set": "raise_syntax_error26",
-    "SyntaxError - missing comma in a list": "raise_syntax_error27",
-    "SyntaxError - missing comma in a tuple": "raise_syntax_error28",
-    "SyntaxError - missing comma between function args": "raise_syntax_error29",
-    "SyntaxError - can't assign to function call - 1": "raise_syntax_error30",
-    "SyntaxError - can't assign to function call - 2": "raise_syntax_error31",
-    "SyntaxError - used equal sign instead of colon": "raise_syntax_error32",
-    "SyntaxError - non-default argument follows default argument": "raise_syntax_error33",
-    "SyntaxError - positional argument follows keyword argument": "raise_syntax_error34",
-    "SyntaxError - f-string: unterminated string": "raise_syntax_error35",
-    "SyntaxError - unclosed bracket": "raise_syntax_error36",
-    "SyntaxError - unexpected EOF while parsing": "raise_syntax_error37",
-    "SyntaxError - name is parameter and global": "raise_syntax_error38",
-    "SyntaxError - keyword as attribute": "raise_syntax_error39",
-    "SyntaxError - content passed continuation line character": "raise_syntax_error40",
-    "SyntaxError - keyword can't be an expression": "raise_syntax_error41",
-    "SyntaxError - invalid character in identifier": "raise_syntax_error42",
-    "SyntaxError - keyword cannot be argument in def - 1": "raise_syntax_error43",
-    "SyntaxError - keyword cannot be argument in def - 2": "raise_syntax_error44",
-    "SyntaxError - keyword cannot be argument in def - 3": "raise_syntax_error45",
-    "SyntaxError - keyword cannot be argument in def - 4": "raise_syntax_error46",
-    "SyntaxError - delete function call": "raise_syntax_error47",
-    "SyntaxError - assigned prior to global declaration": "raise_syntax_error48",
-    "SyntaxError - used prior to global declaration": "raise_syntax_error49",
-    "SyntaxError - assigned prior to nonlocal declaration": "raise_syntax_error50",
-    "SyntaxError - used prior to nonlocal declaration": "raise_syntax_error51",
-    "SyntaxError - named assignment with Python constant": "raise_syntax_error55",
-    "SyntaxError - assignment to operator": "raise_syntax_error56",
-    "SyntaxError - using the backquote character": "raise_syntax_error57",
-    "SyntaxError - assign to generator expression": "raise_syntax_error58",
-    "SyntaxError - assign to conditional expression": "raise_syntax_error59",
-    "SyntaxError - name is parameter and nonlocal": "raise_syntax_error60",
-    "SyntaxError - name is global and nonlocal": "raise_syntax_error61",
-    "SyntaxError - nonlocal variable not found": "raise_syntax_error62",
-    "SyntaxError - nonlocal variable not found at module level": "raise_syntax_error63",
-    "SyntaxError - keyword arg only once in function definition": "raise_syntax_error64",
-    "SyntaxError - keyword arg only once in function call": "raise_syntax_error65",
-    "SyntaxError - unexpected EOF while parsing 2": "raise_syntax_error66",
-    "SyntaxError - print is a function 2": "raise_syntax_error67",
-    "SyntaxError - copy/paste from interpreter": "raise_syntax_error68",
-    "SyntaxError - Using pip from interpreter": "raise_syntax_error69",
-    "SyntaxError - Using pip from interpreter 2": "raise_syntax_error70",
-    "SyntaxError - dot followed by parenthesis": "raise_syntax_error71",
-    "SyntaxError - cannot assign to f-string": "raise_syntax_error72",
-    "SyntaxError - raising multiple exceptions": "raise_syntax_error73",
-    "SyntaxError - parenthesis around generator expression": "raise_syntax_error74",
-    "SyntaxError - invalid character (bad quote)": "raise_syntax_error75",
-    "SyntaxError - single = instead of double == with if": "raise_syntax_error76",
-    "SyntaxError - single = instead of double == with elif": "raise_syntax_error77",
-    "SyntaxError - single = instead of double == with while": "raise_syntax_error78",
-    "SyntaxError - forgot a comma in an f-string": "raise_syntax_error79",
-    "SyntaxError - Valid names cannot begin with a number": "raise_syntax_error80",
-    "SyntaxError - unclosed parenthesis - 3": "raise_syntax_error81",
-    "SyntaxError - forgot a multiplication operator": "raise_syntax_error82",
-    "SyntaxError - space between names": "raise_syntax_error83",
-    "SyntaxError - can't use star operator": "raise_syntax_error84",
-    "SyntaxError - can't use double star operator": "raise_syntax_error85",
-    "SyntaxError - can't use return outside function": "raise_syntax_error86",
-    "SyntaxError - too many nested blocks": "raise_syntax_error87",
-}
-
-if sys.version_info < (3, 8):
-    all_imports["Walrus operator does not exist - yet"] = "raise_syntax_error_walrus"
-
 cur_dir = os.getcwd()
 sys.path.append(os.path.join(cur_dir, "syntax"))
 
@@ -132,20 +40,14 @@ def create_tracebacks(target, intro_text, format="pre"):
         with redirect_stderr(out):
             write(intro_text)
 
-            for title in all_imports:
+            for name in descriptions:
                 function = None
-                if isinstance(all_imports[title], tuple):
-                    name, function = all_imports[title]
-                else:
-                    name = all_imports[title]
+                title = descriptions[name]["title"]
                 make_title(title, format=format)
                 try:
                     mod = __import__(name)
-                    if function is not None:
-                        result = getattr(mod, function)()
-                        write(result)
                 except Exception:
                     friendly_traceback.explain_traceback()
 
 
-print("    Number of cases in trb_syntax_common.py: ", len(all_imports))
+print("    Number of cases in trb_syntax_common.py: ", len(descriptions))
