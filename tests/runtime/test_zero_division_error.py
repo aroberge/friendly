748c07a24f18495459e1c4cb3fcd47066eece5e6
André Roberge <andre.roberge@gmail.com>
2021-01-28 17:47:29 -0400

    Added test case for ZeroDivisionError.

--- tests/runtime/test_zero_division_error.py
+++ tests/runtime/test_zero_division_error.py
@@ -2,8 +2,9 @@ import friendly_traceback
 
 
 def test_Division_operator():
+    zero = 0
     try:
-        1 / 0
+        1 / zero
     except Exception as e:
         message = str(e)
         friendly_traceback.explain_traceback(redirect="capture")
@@ -30,6 +31,21 @@ def test_Modulo_operator():
     return result, message
 
 
+def test_Raise_zero_negative_power():
+    zero = 0
+    try:
+        zero ** -1
+    except Exception as e:
+        message = str(e)
+        friendly_traceback.explain_traceback(redirect="capture")
+    result = friendly_traceback.get_output()
+
+    assert "cannot be raised to a negative power" in result
+    if friendly_traceback.get_lang() == "en":
+        assert "if you try to raise 0 to a negative power" in result
+    return result, message
+
+
 if __name__ == "__main__":
-    print(test_Divison_operator()[0])
+    print(test_Division_operator()[0])
 
