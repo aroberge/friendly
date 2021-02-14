d2b19065a6171fa1238854596d982636f69973f2
André Roberge <andre.roberge@gmail.com>
2021-02-14 14:01:09 -0400

    ModuleNotFoundError: better use of new cause style.

--- friendly_traceback/runtime_errors/module_not_found_error.py
+++ friendly_traceback/runtime_errors/module_not_found_error.py
@@ -24,20 +24,12 @@ def _get_cause(value, *_args):
     pattern = re.compile(r"No module named '(.*)'; '(.*)' is not a package")
     match = re.search(pattern, message)
     if match:
-        cause, hint = is_not_a_package(match.group(1), match.group(2))
-        cause = {"cause": cause}
-        if hint:
-            cause["suggest"] = hint
-        return cause
+        return is_not_a_package(match.group(1), match.group(2))
 
     pattern = re.compile(r"No module named '(.*)'")
     match = re.search(pattern, message)
     if match:
-        cause, hint = no_module_named(match.group(1))
-        cause = {"cause": cause}
-        if hint:
-            cause["suggest"] = hint
-        return cause
+        return no_module_named(match.group(1))
 
     return {"cause": no_information()}
 
@@ -46,12 +38,11 @@ def no_module_named(name):
     _ = current_lang.translate
     similar = get_similar_words(name, stdlib_modules.names)
 
-    hint = None
     cause = _(
         "The name of the module that could not be imported is `{name}`.\n"
     ).format(name=name)
     if not similar:
-        return cause, hint
+        return {"cause": cause}
 
     hint = _("Did you mean `{name}`?\n").format(name=similar[0])
 
@@ -65,12 +56,11 @@ def no_module_named(name):
             name=similar[0]
         )
 
-    return cause, hint
+    return {"cause": cause, "suggest": hint}
 
 
 def is_not_a_package(dotted_path, name):
     _ = current_lang.translate
-    hint = None
 
     rest = dotted_path.replace(name + ".", "")
 
@@ -85,7 +75,7 @@ def is_not_a_package(dotted_path, name):
         debug_helper.log("Problem in is_not_a_package()")
         debug_helper.log(str(e))
         debug_helper.log(cause)
-        return cause, hint
+        return {"cause": cause}
 
     attributes = dir(module)
 
@@ -96,7 +86,7 @@ def is_not_a_package(dotted_path, name):
         cause = _(
             "`{rest}` is not a separate module but an object that is part of `{name}`.\n"
         ).format(name=name, rest=rest)
-        return cause, hint
+        return {"cause": cause, "suggest": hint}
 
     similar = get_similar_words(rest, attributes)
     if similar:
@@ -128,9 +118,10 @@ def is_not_a_package(dotted_path, name):
                 "Other objects with similar names that are part of\n"
                 " `{name}` include `{others}`.\n"
             ).format(name=name, others=list_to_string(similar[1:]))
+            return {"cause": cause, "suggest": hint}
     else:
         cause = _("`{rest}` cannot be imported from `{name}`.\n").format(
             rest=rest, name=name
         )
 
-    return cause, hint
+    return {"cause": cause}
